e8429ded859b40f93efeeeb23015029ea2d904a6
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-04-05 19:11:49 -0300

    build: add tailwindcss config

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,8 @@
 export function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex justify-center">
+      <h1 className="bg-orange-400 font-bold p-2 rounded">Hello World</h1>
+    </div>
   )
 }
