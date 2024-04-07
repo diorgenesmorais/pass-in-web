4ba41911bf81eb52dec98e6e2024e85ef6490b2b
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-04-07 20:08:41 -0300

    feat: add buttons footer

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,35 +1,35 @@
-import { Search } from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
 
 export const AttendeeList = () => {
     return (
         <div className='flex flex-col gap-4'>
             <div className="flex gap-3 items-center">
                 <h1 className="text-2xl font-bold">Participantes</h1>
-                <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+                <div className="w-72 px-3 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className='size-4 text-emerald-300' />
-                    <input type="text" placeholder="Buscar participante..." className="bg-transparent flex-1 outline-none" />
+                    <input type="text" placeholder="Buscar participante..." className="bg-transparent h-9 focus:right-0 flex-1 outline-none border-0 p-0 text-sm" />
                 </div>
             </div>
             <div className='border border-white/10 rounded-lg'>
                 <table className='w-full'>
                     <thead>
                         <tr className='border-b border-white/10'>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>
-                                <input type="checkbox" />
+                            <th style={{width: 48}} className='py-3 px-4 text-sm font-semibold text-left'>
+                                <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
                             </th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Participante</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Data da inscrição</th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>Data do check-in</th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'></th>
+                            <th style={{width: 64}} className='py-3 px-4 text-sm font-semibold text-left'></th>
                         </tr>
                     </thead>
                     <tbody>
                         {Array.from({length: 10}).map((_, i) => {
                             return (
-                                <tr key={i} className='border-b border-white/10'>
+                                <tr key={i} className='border-b border-white/10 hover:bg-white/5'>
                                     <td className='py-3 px-4 text-sm text-left text-zinc-300'>
-                                        <input type="checkbox" />
+                                        <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
                                     </td>
                                     <td className='py-3 px-4 text-sm text-left text-zinc-300'>2465187</td>
                                     <td className='py-3 px-4 text-sm text-left text-zinc-300'>
@@ -40,7 +40,11 @@ export const AttendeeList = () => {
                                     </td>
                                     <td className='py-3 px-4 text-sm text-left text-zinc-300'>7 dias atrás</td>
                                     <td className='py-3 px-4 text-sm text-left text-zinc-300'>3 dias atrás</td>
-                                    <td className='py-3 px-4 text-sm text-left text-zinc-300'></td>
+                                    <td className='py-3 px-4 text-sm text-left text-zinc-300'>
+                                        <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                                            <MoreHorizontal className='size-4' />
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         })}
@@ -50,8 +54,25 @@ export const AttendeeList = () => {
                             <td className='py-3 px-4 text-sm text-left text-zinc-300' colSpan={3}>
                                 Mostrando 10 de 228 itens
                             </td>
-                            <td className='py-3 px-4 text-sm text-left text-zinc-300 text-right' colSpan={3}>
-                                Página 1 de 23
+                            <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
+                                <div className='inline-flex items-center gap-8'>
+                                    <span>Página 1 de 23</span>
+                                    <div className='flex gap-1.5'>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronsLeft className='size-4' />
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronLeft className='size-4' />
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronRight className='size-4' />
+                                        </button>
+                                        <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                            <ChevronsRight className='size-4' />
+                                        </button>
+                                    </div>
+                                </div>
+                                
                             </td>
                         </tr>
                     </tfoot>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+/*global require*/
+/*eslint no-undef: "error"*/
 export default {
   content: [
     "./index.html",
@@ -7,5 +9,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
